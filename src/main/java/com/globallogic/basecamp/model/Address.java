7b4b582c7cd4c0cb037983b3e431e1ffab2c672f
Dmytro <umalanch@gmail.com>
2021-09-27 21:08:41 +0300

    test1

--- src/main/java/com/globallogic/basecamp/model/Address.java
+++ src/main/java/com/globallogic/basecamp/model/Address.java
@@ -5,25 +5,45 @@ package com.globallogic.basecamp.model;
  */
 public class Address {
 
-    private final String country;
+    private String country;
 
-    private final String city;
+    private String city;
 
-    private final String street;
+    private String street;
 
-    private final Integer houseNumber;
+    private Integer houseNumber;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public Integer getHouseNumber() {
+        return houseNumber;
+    }
 
     /**
      * TODO: implement getters and other methods if necessary
      */
+
 
     /**
      * Allows to get the builder for the Address
      *
      * @return builder
      */
-    public static Builder builder() {
-        
+    private Address(Builder builder) {
+        this.country = builder.country;
+        this.city = builder.city;
+        this.street = builder.street;
+        this.houseNumber = builder.houseNumber;
     }
 
     /**
@@ -32,7 +52,40 @@ public class Address {
      * <p>
      * TODO: implement the builder functionality
      */
-    public class Builder {
-    }
+    static class Builder {
+        private String country;
+        private String city;
+        private String street;
+        private Integer houseNumber;
 
-}
+        public Builder(String country, String city, String street, Integer houseNumber) {
+            this.country = country;
+            this.city = city;
+            this.street = street;
+            this.houseNumber = houseNumber;
+        }
+
+        public Builder setCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Builder setCity(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder setStreet(String street) {
+            this.street = street;
+            return this;
+        }
+
+        public Builder setHouseNumber(Integer houseNumber) {
+            this.houseNumber = houseNumber;
+            return this;
+        }
+        public Address build(){
+           return new Address(this);
+        }
+    }
+    }
