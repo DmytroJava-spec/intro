8ebc852cb16abd28c0e3cc053888b8358b07f3ec
Dmytro <umalanch@gmail.com>
2021-09-28 00:37:18 +0300

    test3

--- src/main/java/com/globallogic/basecamp/comparator/StudentComparator.java
+++ src/main/java/com/globallogic/basecamp/comparator/StudentComparator.java
@@ -7,12 +7,19 @@ import java.util.Comparator;
 /**
  * Custom comparator for the {@link com.globallogic.basecamp.model.Student Student} class
  */
-public class StudentComparator implements Comparator<Student> {
+    class StudentSecondNameComparator implements Comparator<Student> {
 
     /**
      * TODO: implement `compare` method of the Comparator interface
      * Compare students in natural order by the last name. If last names are the same,
      * compare in natural order by the first name
      */
-
+        public int compare(Student a,Student b){
+            return a.getLastName().compareTo(b.getLastName());
+        }
+}
+    class StudentFirstNameComparator implements Comparator<Student>{
+    public int compare(Student a, Student b){
+        return a.getFirstName().compareTo(b.getFirstName());
+    }
 }
